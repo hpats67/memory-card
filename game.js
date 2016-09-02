157ddf02e4c6ffdb72076be4e57b4e997c6ed155
TomTimmel <tom.t.timmel@gmail.com>
2016-09-02 13:25:42 -0700

    first logic

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
 'use strict';
+
+var imageArray = ['dog', 'dog', 'cat', 'cat'];
+var altImage;
+
+function shuffleArray(array){
+  for (var i = imageArray.length - 1; i > 0; i--){
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+var check = shuffleArray(imageArray);
+console.log(check);
